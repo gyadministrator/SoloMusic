fdb943f67cf7842caa70d1b5e17e73bde596a0b6
gy <1984629668@qq.com>
2019-12-11 00:00:49 +0800

    加入环信。

--- app/src/main/java/com/example/gy/musicgame/fragment/InfoFragment.java
+++ app/src/main/java/com/example/gy/musicgame/fragment/InfoFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -16,15 +17,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.gy.musicgame.R;
+import com.example.gy.musicgame.adapter.MyPagerAdapter;
 import com.example.gy.musicgame.constant.Constants;
+import com.example.gy.musicgame.fragment.info.MessageFragment;
+import com.example.gy.musicgame.fragment.info.NoticeFragment;
 import com.example.gy.musicgame.model.BottomBarVo;
 import com.example.gy.musicgame.utils.SharedPreferenceUtil;
 import com.example.gy.musicgame.view.BottomBarView;
 import com.example.gy.musicgame.view.TitleView;
+import com.google.android.material.tabs.TabLayout;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InfoFragment extends Fragment {
 
@@ -32,6 +39,8 @@ public class InfoFragment extends Fragment {
     private Activity mActivity;
     private TitleView titleView;
     private BottomBarView bottomBarView;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     public static InfoFragment newInstance() {
         return new InfoFragment();
@@ -52,12 +61,21 @@ public class InfoFragment extends Fragment {
     }
 
     private void initData() {
-
+        List<Fragment> fragmentList = new ArrayList<>();
+        List<String> list_Title = new ArrayList<>();
+        fragmentList.add(NoticeFragment.newInstance());
+        fragmentList.add(MessageFragment.newInstance());
+        list_Title.add("通知");
+        list_Title.add("消息");
+        viewPager.setAdapter(new MyPagerAdapter(getFragmentManager(), mActivity, fragmentList, list_Title));
+        tabLayout.setupWithViewPager(viewPager);//此方法就是让tabLayout和ViewPager联动
     }
 
     private void initView(View view) {
-        titleView= view.findViewById(R.id.titleView);
-        bottomBarView= view.findViewById(R.id.bottom_bar_view);
+        titleView = view.findViewById(R.id.titleView);
+        bottomBarView = view.findViewById(R.id.bottom_bar_view);
+        viewPager = view.findViewById(R.id.viewPager);
+        tabLayout = view.findViewById(R.id.tabLayout);
     }
 
     @Override
---
+++ app/src/main/java/com/example/gy/musicgame/fragment/info/NoticeFragment.java
@@ -0,0 +1,38 @@
+package com.example.gy.musicgame.fragment.info;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gy.musicgame.R;
+
+public class NoticeFragment extends Fragment {
+
+    private NoticeViewModel mViewModel;
+
+    public static NoticeFragment newInstance() {
+        return new NoticeFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.notice_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(NoticeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
